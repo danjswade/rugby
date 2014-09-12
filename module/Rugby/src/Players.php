0f72cd5d3136a60ebb9177121c03f63e22b83858
danjswade <dansjwade@gmail.com>
2014-09-12 21:33:12 +0100

    Created classes, add properties to Team

---
+++ module/Rugby/src/Players.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @Created by Dan Wade <dan@danwade-development.co.uk>
+ * @Link http://www.danwade-development.co.uk
+ * @Date: 12/09/14
+ * @Time: 21:20
+ */
+
+namespace Rugby\src;
+
+
+class Players {
+
+} 
---
+++ module/Rugby/src/Team.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @Created by Dan Wade <dan@danwade-development.co.uk>
+ * @Link http://www.danwade-development.co.uk
+ * @Date: 12/09/14
+ * @Time: 21:18
+ */
+
+namespace Rugby\src;
+
+
+class Team {
+
+} 
