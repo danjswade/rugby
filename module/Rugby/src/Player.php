780f034c9b8ce9f7d9f6711acf2b521b71658ddc
danjswade <dansjwade@gmail.com>
2014-09-12 21:49:47 +0100

    Altered names of classes. Updated functions in Player

---
+++ module/Rugby/src/Player.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @Created by Dan Wade <dan@danwade-development.co.uk>
+ * @Link http://www.danwade-development.co.uk
+ * @Date: 12/09/14
+ * @Time: 21:20
+ */
+
+namespace Rugby;
+
+
+class Players {
+
+    protected $name;
+    protected $dob;
+    protected $position;
+
+    /**
+     * @param mixed $dob
+     */
+    public function setDob($dob)
+    {
+        $this->dob = $dob;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDob()
+    {
+        return $this->dob;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $position
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+
+
+}
+
+?>
