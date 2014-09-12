040cb6e4afb53bc91b046a2b426c820d7ce7cd01
danjswade <dansjwade@gmail.com>
2014-09-12 22:11:49 +0100

    emptied array and created const in Team

--- module/Rugby/src/Team.php
+++ module/Rugby/src/Team.php
@@ -12,27 +12,27 @@ namespace Rugby;
 class Team
 {
 
-    protected $selection = array(
-        "loose" => "",
-        "hooker" => "",
-        "tight" => "",
-        "leftlock" => "",
-        "rightlock" => "",
-        "blindside" => "",
-        "openside" => "",
-        "eight" => "",
-        "scrumhalf" => "",
-        "flyhalf" => "",
-        "lwing" => "",
-        "inside" => "",
-        "outside" => "",
-        "rwing" => "",
-        "fullback" => "",
-        "sub1" => "",
-        "sub2" => "",
-        "sub3" => "",
-        "sub4" => "",
-        "sub5" => ""
-    );
+    protected $selection = array();
+
+    const POSITION_LOOSE = 'Loose';
+    const POSITION_HOOKER = 'Hooker';
+    const POSITION_TIGHT = 'Tight';
+    const POSITION_LEFTLOCK = 'Leftlock';
+    const POSITION_RIGHTLOCK = 'Rightlock';
+    const POSITION_BLIND = 'Blind';
+    const POSITION_OPEN = 'Open';
+    const POSITION_EIGHT = 'Eight';
+    const POSITION_SCRUMHALF = 'Scrumhalf';
+    const POSITION_FLYHALF = 'Flyhalf';
+    const POSITION_LWING = 'Lwing';
+    const POSITION_INSIDE = 'Inside';
+    const POSITION_OUTSIDE = 'Outside';
+    const POSITION_RWING = 'Rwing';
+    const POSITION_FULLBACK = 'Fullback';
+    const POSITION_SUB1 = 'Sub1';
+    const POSITION_SUB2 = 'Sub2';
+    const POSITION_SUB3 = 'Sub3';
+    const POSITION_SUB4 = 'Sub4';
+    const POSITION_SUB5 = 'Sub5';
 
 } 
